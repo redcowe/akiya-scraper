a73090a6581bed5a9466bdbb5566229a21b47096
Joshua Cowell <joshuacowell@hotmail.com>
2022-05-24 15:35:44 +0000

    Started messing with colly examples

---
+++ scrapper.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
+
+func main() {
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("tokyodev.com"),
+	)
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://www.tokyodev.com")
+}
